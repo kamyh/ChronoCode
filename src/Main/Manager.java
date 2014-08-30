0194bfb3ec8051f47848e04685814aad2451b01f
kamyh <deruazvincent@gmail.com>
2014-08-30 13:03:42 +0200

    add frame. add two new TODO.

--- src/Main/Manager.java
+++ src/Main/Manager.java
@@ -1,16 +1,27 @@
 package Main;
 
+import javax.swing.JFrame;
+
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.ptr.PointerByReference;
 
-public class Manager {
+public class Manager extends JFrame {
 
 	public Manager() {
+		super("Chrono Code");
+		apperence();
+
 		this.isStart = true;
 		init();
 		start();
+	}
+
+	private void apperence() {
+
+		this.setVisible(true);
+		this.setSize(800, 600);
 	}
 
 	public void start() {
@@ -35,25 +46,34 @@ public class Manager {
 				System.out.println(Native.toString(buffer) + ":"
 						+ this.tasks[0].getBaseName());
 
-				if (this.tasks[0].getBaseName().equals(Native.toString(buffer))) {
-					this.tasks[0]
-							.setElapsedTime(this.tasks[0].getElapsedTime() + 1);
-					System.out.println(this.tasks[0].getElapsedTime());
-				}
-				if (this.tasks[1].getBaseName().equals(Native.toString(buffer))) {
-					this.tasks[1]
-							.setElapsedTime(this.tasks[1].getElapsedTime() + 1);
-					System.out.println(this.tasks[1].getElapsedTime());
+				for (int i = 0; i < this.tasks.length; i++) {
+
+					if (this.tasks[i].getBaseName().equals(
+							Native.toString(buffer))) {
+						this.tasks[i].setElapsedTime(this.tasks[i]
+								.getElapsedTime() + refreshTime);
+						System.out
+								.println(this.tasks[i].getElapsedTime() / 1000);
+					}
 				}
 
 				try {
 					Thread.sleep(refreshTime);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
+					System.err
+							.println("Impossible to sleep. too many noises !");
 					e.printStackTrace();
 				}
 			}
 		}
+	}
+
+	private void save(String path) {
+		// TODO
+	}
+
+	private void load(String path) {
+		// TODO
 	}
 
 	private void listAllAppRunning() {
@@ -61,11 +81,11 @@ public class Manager {
 	}
 
 	private void setWhiteList() {
-
+		// TODO
 	}
 
 	private void init() {
-		this.tasks = new Task[2];
+		this.tasks = new Task[3];
 
 		for (int i = 0; i < this.tasks.length; i++) {
 			this.tasks[i] = new Task();
@@ -74,8 +94,11 @@ public class Manager {
 		this.tasks[0].setName("Eclipse");
 		this.tasks[0].setBaseName("eclipse.exe");
 
-		this.tasks[1].setName("Chrome");
-		this.tasks[1].setBaseName("chrome.exe");
+		this.tasks[1].setName("Eclipse");
+		this.tasks[1].setBaseName("javaw.exe");
+
+		this.tasks[2].setName("Chrome");
+		this.tasks[2].setBaseName("chrome.exe");
 
 	}
 
