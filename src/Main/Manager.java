60328316846e9f6fadea65965f76e8207e6c5d18
kamyh <deruazvincent@gmail.com>
2014-08-31 23:07:18 +0200

    Add some GUI

--- src/Main/Manager.java
+++ src/Main/Manager.java
@@ -1,6 +1,15 @@
 package Main;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
@@ -21,59 +30,114 @@ public class Manager extends JFrame {
 
 	private void control() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// menuBar
+		this.menuBar = new JMenuBar();
+		this.setJMenuBar(this.menuBar);
+		// Define and add two drop down menu to the menubar
+		JMenu fileMenu = new JMenu("File");
+		JMenu actionMenu = new JMenu("Actions");
+		menuBar.add(fileMenu);
+		menuBar.add(actionMenu);
+		JMenuItem newAction = new JMenuItem("New");
+		JMenuItem openAction = new JMenuItem("Open");
+		JMenuItem exitAction = new JMenuItem("Exit");
+		JMenuItem startPauseAction = new JMenuItem("Start/Pause");
+		fileMenu.add(newAction);
+		fileMenu.add(openAction);
+		fileMenu.add(exitAction);
+		actionMenu.add(startPauseAction);
+
+		// TODO attributs action to menus
+		newAction.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("You have clicked on the new action");
+			}
+		});
+
+		startPauseAction.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				isStart = !isStart;
+				System.out.println("isStart: " + isStart);
+			}
+		});
+
 	}
 
 	private void apperence() {
 
 		this.setVisible(true);
 		this.setSize(800, 600);
+
+		this.mainBorderLayout = new BorderLayout();
+
+		this.mainJPanel = new JPanel(this.mainBorderLayout);
+
+		this.add(this.mainJPanel);
+
+		// this.jComboBoxListProcess = new JComboBox();
+		// this.mainBorderLayout.addLayoutComponent(this.jComboBoxListProcess,
+		// "North");
+
 	}
 
 	public void start() {
 		// TODO put in a thread
-		while (true) {
-			if (this.isStart) {
-				char[] buffer = new char[MAX_TITLE_LENGTH * 2];
-				User32DLL.GetWindowTextW(User32DLL.GetForegroundWindow(),
-						buffer, MAX_TITLE_LENGTH);
-				// System.out.println("Active window title: "
-				// + Native.toString(buffer));
 
-				PointerByReference pointer = new PointerByReference();
-				User32DLL.GetWindowThreadProcessId(
-						User32DLL.GetForegroundWindow(), pointer);
-				Pointer process = Kernel32.OpenProcess(
-						Kernel32.PROCESS_QUERY_INFORMATION
-								| Kernel32.PROCESS_VM_READ, false,
-						pointer.getValue());
-				Psapi.GetModuleBaseNameW(process, null, buffer,
-						MAX_TITLE_LENGTH);
+		threadCheckFocus = new Thread(new Runnable() {
 
-				for (int i = 0; i < this.session.getTasks().size(); i++) {
+			@Override
+			public void run() {
 
-					if (this.session.getTasks().get(i).getBaseName()
-							.equals(Native.toString(buffer))) {
-						this.session
-								.getTasks()
-								.get(i)
-								.setElapsedTime(
-										this.session.getTasks().get(i)
-												.getElapsedTime()
-												+ refreshTime);
-						System.out.println(this.session.getTasks().get(i)
-								.getElapsedTime() / 1000);
+				while (true) {
+					System.out.println("isStart From Thread:" + isStart);
+					if (isStart) {
+						char[] buffer = new char[MAX_TITLE_LENGTH * 2];
+						User32DLL.GetWindowTextW(
+								User32DLL.GetForegroundWindow(), buffer,
+								MAX_TITLE_LENGTH);
+						// System.out.println("Active window title: "
+						// + Native.toString(buffer));
+
+						PointerByReference pointer = new PointerByReference();
+						User32DLL.GetWindowThreadProcessId(
+								User32DLL.GetForegroundWindow(), pointer);
+						Pointer process = Kernel32.OpenProcess(
+								Kernel32.PROCESS_QUERY_INFORMATION
+										| Kernel32.PROCESS_VM_READ, false,
+								pointer.getValue());
+						Psapi.GetModuleBaseNameW(process, null, buffer,
+								MAX_TITLE_LENGTH);
+
+						for (int i = 0; i < session.getTasks().size(); i++) {
+
+							if (session.getTasks().get(i).getBaseName()
+									.equals(Native.toString(buffer))) {
+								session.getTasks()
+										.get(i)
+										.setElapsedTime(
+												session.getTasks().get(i)
+														.getElapsedTime()
+														+ refreshTime);
+								System.out.println(session.getTasks().get(i)
+										.getElapsedTime() / 1000);
+							}
+						}
+					}
+					try {
+						Thread.sleep(refreshTime);
+					} catch (InterruptedException e) {
+						System.err
+								.println("Impossible to sleep. too many noises !");
+						e.printStackTrace();
 					}
 				}
-
-				try {
-					Thread.sleep(refreshTime);
-				} catch (InterruptedException e) {
-					System.err
-							.println("Impossible to sleep. too many noises !");
-					e.printStackTrace();
-				}
 			}
-		}
+		});
+
+		this.threadCheckFocus.start();
+
 	}
 
 	private void save(String path) {
@@ -104,8 +168,13 @@ public class Manager extends JFrame {
 
 	private static final int MAX_TITLE_LENGTH = 1024;
 	private int refreshTime = 1000;
-	private boolean isStart;
+	private volatile boolean isStart;
 	private Session session;
+	private Thread threadCheckFocus;
+	JMenuBar menuBar;
+	private BorderLayout mainBorderLayout;
+	private JPanel mainJPanel;
+	private JComboBox jComboBoxListProcess;
 
 	static class Psapi {
 		static {
